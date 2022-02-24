ace393b22cae782419d29b88ac927768490a9d5b
hee1won <happen23@naver.com>
2022-02-24 12:16:20 +0900

    20220204

---
+++ src/chap01/CodingTip.java
@@ -0,0 +1,26 @@
+package chap01;
+
+public class CodingTip {
+	// hint 단축키 : ctrl + space
+	
+	// main 메소드 코드 단축 키워드 : main
+	public static void main(String[] args) {
+
+		// 자동코드포맷팅(줄맞춤) : ctrl(command) + shift + f
+		
+		// 콘솔 출력
+		System.out.println("출력할 텍스트");
+		
+		// 콘솔 출력 코드 단축 키워드 : sysout, sout
+		
+		// 주석 : ctrl + /
+		
+		// 창크기변경 : ctrl + m
+		
+		// 새파일 : ctrl + n
+		
+		// 저장 : ctrl + s
+		
+	}
+
+}
---
+++ src/chap01/Hello.java
@@ -0,0 +1,16 @@
+package chap01;
+
+/*
+ * 작성자 : 노희원
+ * 작성일 : 2022.02.24
+ */
+public class Hello {
+	//프로그램 실행 진입
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행
+		System.out.println("Hello, welcome to the java world!");
+		
+		// 단축키 ctrl + /
+	} //end of main
+
+} //end of class
