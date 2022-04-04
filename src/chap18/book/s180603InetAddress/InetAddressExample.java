c457c28a17ae5b7db83c049bd5c58a701178b6d8
hee1won <happen23@naver.com>
2022-04-05 01:14:20 +0900

    1

---
+++ src/chap18/book/s180603InetAddress/InetAddressExample.java
@@ -0,0 +1,21 @@
+package chap18.book.s180603InetAddress;
+
+import java.net.InetAddress;
+
+public class InetAddressExample {
+	public static void main(String[] args) {
+		
+		try {
+			InetAddress local = InetAddress.getLocalHost();
+			System.out.println("내컴퓨터 ip주소 : " + local.getHostAddress());
+			
+			InetAddress[]iaArr = InetAddress.getAllByName("www.naver.com");
+			for(InetAddress remote : iaArr) {
+				System.out.println("www.naver.com IP주소 : " + remote.getHostAddress());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
